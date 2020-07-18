18917ffece57c0534e30faabd5ab57f2436e4cf1
Andrean Lay <50937000+andreanlay@users.noreply.github.com>
2020-07-18 15:51:31 +0700

    Add data augmentation

--- training.py
+++ training.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization
 from keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 
 def load_data():
@@ -71,15 +72,32 @@ def preprocessing(x_train, x_test):
 
 x_train, x_test, y_train, y_test = load_data()
 (x_train, x_test) = preprocessing(x_train, x_test)
+# Initialize Keras image generator
+generator = ImageDataGenerator(rotation_range=10, zoom_range=0.1, width_shift_range=0.1, height_shift_range=0.1)
+
 # CNN Model
 model = Sequential([
-    Conv2D(filters=64, kernel_size=(5, 5), input_shape=(28, 28, 1), activation="relu"),
-    Conv2D(filters=32, kernel_size=(3, 3), activation="relu"),
-    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(filters=32, kernel_size=3, input_shape=(28, 28, 1), activation="relu"),
+    BatchNormalization(),
+    Conv2D(filters=32, kernel_size=3, activation="relu"),
+    BatchNormalization(),
+    Conv2D(filters=32, kernel_size=5, strides=2, padding="same", activation="relu"),
+    BatchNormalization(),
+    Dropout(0.4),
+    Conv2D(filters=64, kernel_size=3, activation="relu"),
+    BatchNormalization(),
+    Conv2D(filters=64, kernel_size=3, activation="relu"),
+    BatchNormalization(),
+    Conv2D(filters=64, kernel_size=5, strides=2, padding="same", activation="relu"),
+    BatchNormalization(),
+    Dropout(0.4),
+    Conv2D(128, kernel_size=4, activation="relu"),
+    BatchNormalization(),
     Flatten(),
+    Dropout(0.4),
     Dense(36, activation="softmax")
 ])
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=512)
+history = model.fit_generator(generator.flow(x_train, y_train, batch_size=64), epochs=30, steps_per_epoch=x_train.shape[0]/64, validation_data=(x_test, y_test))
